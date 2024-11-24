e1393350bba994073a75cffe689cb54898797115
nin0dev <personal@nin0.dev>
2024-11-24 11:08:44 -0500

    more guh

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Client } from "oceanic.js";
 import { Embedder } from "./utils/types";
 import { parse } from "./parser";
+import { readdir } from "fs/promises";
+import { Yapper } from "./utils/Yapper";
 
 export const client = new Client({
 	auth: `Bot ${process.env.BOT_TOKEN}`,
@@ -9,16 +11,30 @@ export const client = new Client({
 	}
 });
 export const embedders: Embedder[] = [];
+export const yapper = new Yapper();
 
 client.on("ready", () => {
-	console.log(`Connected as ${client.user.tag}!`);
+	yapper.info(`Connected as ${client.user.tag}!`);
 });
 
 client.once("ready", async () => {
-	console.log("Loading embedders...");
-	const embedder = await import("./embedders/spotify/track");
-	embedders.push(embedder.default);
-	console.log("Loaded all embedders!");
+	const fileList = (
+		await readdir(
+			`./${process.env.NODE_ENV === "production" ? "dist/" : "src/"}embedders/`,
+			{
+				recursive: true
+			}
+		)
+	).filter(file => file.endsWith(".ts") || file.endsWith(".js"));
+	yapper.debug(`Preparing load of ${fileList.length} embedders...`);
+	for (const file of fileList) {
+		const embedder = await import(`./embedders/${file}`);
+		embedders.push(embedder.default);
+		yapper.debug(
+			`Loaded ${embedder.default.linkType} - ${embedder.default.service.name}`
+		);
+	}
+	yapper.info("Loaded all embedders!");
 });
 
 client.on("messageCreate", async message => {
@@ -40,7 +56,7 @@ client.on("messageCreate", async message => {
 });
 
 process.on("uncaughtException", e => {
-	console.error("💢 Uncaught exception:", e);
+	yapper.error(e);
 });
 process.on("unhandledRejection", e => {
 	console.error("💢 Unhandled rejection:", e);
